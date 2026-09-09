5c06cae3e6a9ad5f8dcfc65047b5415d9db7f2ca
paramjeet-dev <paramjeetahlawat31@gmail.com>
2026-09-09 13:35:56 +0530

    test

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -32,7 +32,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
     const data = await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
+      { body: [] }
     );
 
     console.log(`Successfully reloaded ${data.length} application (/) commands.`);
